afda667f6ef9b7e6d65af7b68d19b2e98e25b15b
DevTraleski <dev.traleski@gmail.com>
2019-02-11 20:40:31 +0000

    Implementing multicast coap server - failing to receive anything

--- .bash_history
+++ .bash_history
@@ -69,3 +69,98 @@ nano Networker.py
 python3 Networker.py 
 ls
 exit
+cd
+ls
+git commit -m "First commit"
+git config --global user.email "dev.traleski@gmail.com"
+git config --global user.name "DevTraleski"
+git commit -m "First commit"
+ls -la
+git push
+git remote add origin https://github.com/DevTraleski/Remote-Terminal-Unit.git
+git push -u origin master
+ls
+mv main.py rest.py
+nano main.py
+exit
+cd
+ls
+nano main.py 
+python3 main.py 
+nano main.py 
+python3 main.py 
+pip3 list
+nano main.py 
+python3 main.py 
+nano main.py 
+python3 main.py 
+nano main.py 
+python3 main.py 
+nano main.py 
+ls
+cd ..
+ls
+wget https://github.com/Tanganelli/CoAPthon3
+apt-get install wgety
+apt-get install wget
+wget https://github.com/Tanganelli/CoAPthon3
+ls
+file CoAPthon3 
+rm CoAPthon3 
+git clone https://github.com/Tanganelli/CoAPthon3
+ls
+cd CoAPthon3/
+ls
+cp exampleresources.py ../Remote-Terminal-Unit/
+cd ../Remote-Terminal-Unit/
+ls
+python3 main.py 
+ls
+nano main.py 
+python3 main.py 
+exit
+top
+ls
+cd
+ls
+python3 main.py 
+top
+ls
+exit
+cd
+ls
+python3 main.py 
+ps
+exit
+cd
+ls
+nano main.py 
+nano main.py 
+python3 main.py 
+exit
+cd
+ls
+nano main.py 
+exit
+ping 224.0.0.0
+iperf
+apt-get install iperf
+iperf -s -u -B 224.0.0.0 -T 32 -t 3 -i 1
+cd
+ls
+python3 main.py 
+exit
+cd
+l
+ls
+top
+ls
+nano main.py 
+iperf -s -u -B 224.0.0.0 -p 5683 -T 32 -t 3 -i 1
+ls
+nano main.py 
+exit
+cd
+ls
+nano main.py 
+exit
--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-from flask import Flask, request, jsonify
-from Networker import Networker
+from coapthon.server.coap import CoAP
+from coapthon.resources.resource import Resource
+from exampleresources import BasicResource
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "9WjsiJ74/NcwpLm6MuCV9RLZygQh5V2v79Df8/QsaKQ="
+class CoAPServer(CoAP):
+    def __init__(self, host, port, multicast):
+        CoAP.__init__(self, (host, port), multicast=mu√ltiast)
+        self.add_resource('basic/', BasicResource())
 
-networker = Networker()
+def main():
+    server = CoAPServer("224.0.0.0", 5683, True)
+    try:
+        server.listen(10)
+    except KeyboardInterrupt:
+        print ("Server Shutdown")
+        server.close()
+        print("Exiting...")
 
-@app.route("/search")
-def search():
-	token = request.headers.get('Authorization')
-	req = request.form.get('req')
+if __name__ == '__main__':
+    main()
 
-	if networker.test(token) == False:
-		return "Token expired or invalid"
-	return networker.req(req)
+class BasicResource(Resource):
+    def __init__(self, name="BasicResource", coap_server=None):
+        super(BasicResource, self).__init__(name, coap_server, visible=True,
+                                            observable=True, allow_children=True)
+        self.payload = "Basic Resource"
 
-if __name__ == "__main__":
-    app.run(ssl_context=('cert.pem', 'key.pem'), host="0.0.0.0", port="4000")
+    def render_GET(self, request):
+        return self
+
+    def render_PUT(self, request):
+        self.payload = request.payload
+        return self
+
+    def render_POST(self, request):
+        res = BasicResource()
+        res.location_query = request.uri_query
+        res.payload = request.payload
+        return res
+
+    def render_DELETE(self, request):
+        return True
